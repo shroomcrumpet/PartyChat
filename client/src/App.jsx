6bf43fda43e76e1217c8ebc7ccb948640b496ea7
shroomcrumpet <kencheng3@gmail.com>
2018-11-20 14:20:18 +0800

    Added react constructor and functions; implemented working chat room

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,42 +7,89 @@ import './App.css';
 
 class App extends Component {
 
-  componentDidMount() {
-    var webrtc = new SimpleWebRTC({
+  constructor() {
+    super();
+
+    this.webrtc = new SimpleWebRTC({
       localVideoEl: 'localVideo',
       remoteVideosEl: 'remoteVideos',
-      autoRequestMedia: true
+      autoRequestMedia: true,
+      nick: 'Kennypoo',
     });
 
-    webrtc.on('readyToCall', function () {
-      webrtc.joinRoom('testRoom');
+    this.handleChatSubmit = this.handleChatSubmit.bind(this);
+    this.addToChat = this.addToChat.bind(this);
+  }
+
+
+  handleChatSubmit(event) {
+    event.preventDefault();
+    var msg = event.target[0].value;
+
+    if (msg) {
+      this.webrtc.sendDirectlyToAll('p2pchat', 'chat', {
+        message: msg,
+      });
+      event.target[0].placeholder = '';
+      event.target[0].value = '';
+      this.addToChat('Me', msg);
+    } else {
+      event.target[0].placeholder = 'Cannot be blank';
+    };
+  }
+
+  addToChat(name, message) {
+    var newText = document.createElement('p');
+    newText.textContent = `${name}: ${message}`;
+    document.querySelector('#text-chat').appendChild(newText);
+  };
+
+
+  componentDidMount() {
+    const reactThis = this;
+
+    this.webrtc.on('readyToCall', function () {
+      reactThis.webrtc.joinRoom('testRoom');
+    });
+
+    this.webrtc.on('channelMessage', function (label, type, data) {
+      // console.log('label: ', label);
+      // console.log('type: ', type);
+      // console.log('data: ', data);
+
+      if (data.type === 'chat') {
+        reactThis.addToChat(label.nick, data.payload.message);
+      };
     });
   }
 
+
   render() {
     return (
-
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
 
         <div className="videoDiv">
           <div>
             <video id="localVideo"></video>
           </div>
-          <div id="remoteVideos"></div>
+
+          <div>
+            <div id="remoteVideos"></div>
+          </div>
+        </div>
+
+        <div className="chatDiv">
+          <div id="text-chat"></div>
+          <form onSubmit={this.handleChatSubmit}>
+            <input id="chat-box" />
+            <input type="submit" value="Send" />
+          </form>
         </div>
 
       </div>
