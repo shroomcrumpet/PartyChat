3a1e438e886d7c860ed6d63ecb6648fb631ee3c6
shroomcrumpet <kencheng3@gmail.com>
2018-11-22 19:11:21 +0800

    Switched to SWRTC API, code refactor with React-Redux

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,111 +1,140 @@
 import React, { Component } from 'react';
 import generateName from 'sillyname';
+import * as RTC from "@andyet/simplewebrtc";
 
 import './App.scss';
 
 
-class App extends Component {
+const App = ({ configUrl, roomName }) => (
 
-  constructor(props) {
-    super(props);
+  <RTC.Provider configUrl={configUrl}>
 
-    this.state = {
-      username: generateName(),
-    };
+    <RTC.RemoteAudioPlayer />
 
-    this.CONFIG_URL = `https://api.simplewebrtc.com/config/guest/${process.env.REACT_APP_RTC_API_KEY}`
+    <RTC.Connecting>
+      <h1>Client is connecting...</h1>
+    </RTC.Connecting>
 
-    // this.webrtc = new SimpleWebRTC({
-    //   localVideoEl: 'localVideo',
-    //   remoteVideosEl: 'remoteVideos',
-    //   autoRequestMedia: true,
-    // });
+    <RTC.Disconnected>
+      <h1>Client lost connection. Attempting to rejoin...</h1>
+    </RTC.Disconnected>
 
-    this.handleChatSubmit = this.handleChatSubmit.bind(this);
-    this.addToChat = this.addToChat.bind(this);
-    this.onChangeName = this.onChangeName.bind(this);
-  };
+    <RTC.Connected>
 
+      <RTC.RequestUserMedia audio video auto />
 
-  handleChatSubmit(event) {
-    event.preventDefault();
-    let msg = event.target[0].value;
+      <RTC.Room name={roomName}>
 
-    // if (msg) {
-    //   this.webrtc.sendDirectlyToAll('p2pchat', 'chat', {
-    //     username: this.state.username,
-    //     message: msg,
-    //   });
-    //   event.target[0].placeholder = '';
-    //   event.target[0].value = '';
-    //   this.addToChat(`${this.state.username} (me)`, msg);
-    // } else {
-    //   event.target[0].placeholder = 'Cannot be blank';
-    // };
-  };
+      </RTC.Room>
 
-  addToChat(name, message) {
-    let newText = document.createElement('p');
-    newText.textContent = `${name}: ${message}`;
-    document.querySelector('#text-chat').appendChild(newText);
-  };
+    </RTC.Connected>
 
-  onChangeName(event) {
-    this.setState({username: event.target.value});
-  };
+  </RTC.Provider>
+);
 
 
-  componentDidMount() {
-    const reactThis = this;
-
-    console.log()
-
-    // this.webrtc.on('readyToCall', function () {
-    //   console.log("Joining room: ", reactThis.props.match.params.roomname);
-    //   reactThis.webrtc.joinRoom(reactThis.props.match.params.roomname);
-    // });
-
-    // this.webrtc.on('channelMessage', function (label, type, data) {
-
-    //   if (data.type === 'chat') {
-    //     reactThis.addToChat(data.payload.username, data.payload.message);
-    //   };
-    // });
-  }
 
 
-  render() {
+// class App extends Component {
 
-    return (
-      <div className="App">
+//   constructor(props) {
+//     super(props);
 
-        <div className="videoDiv">
-          <div>
-            <video id="localVideo"></video>
-          </div>
+//     this.state = {
+//       username: generateName(),
+//     };
 
-          <div>
-            <div id="remoteVideos"></div>
-          </div>
-        </div>
+//     // this.webrtc = new SimpleWebRTC({
+//     //   localVideoEl: 'localVideo',
+//     //   remoteVideosEl: 'remoteVideos',
+//     //   autoRequestMedia: true,
+//     // });
 
-        <div className="chatDiv">
+//     this.handleChatSubmit = this.handleChatSubmit.bind(this);
+//     this.addToChat = this.addToChat.bind(this);
+//     this.onChangeName = this.onChangeName.bind(this);
+//   };
 
-          <div id="text-chat"></div>
 
-          <span>Your name: </span>
-          <input onChange={this.onChangeName} value={this.state.username} />
+//   handleChatSubmit(event) {
+//     event.preventDefault();
+//     let msg = event.target[0].value;
 
-          <form onSubmit={this.handleChatSubmit}>
-            <input id="chat-box" placeholder="Enter chat message" />
-            <input type="submit" value="Send" />
-          </form>
+//     // if (msg) {
+//     //   this.webrtc.sendDirectlyToAll('p2pchat', 'chat', {
+//     //     username: this.state.username,
+//     //     message: msg,
+//     //   });
+//     //   event.target[0].placeholder = '';
+//     //   event.target[0].value = '';
+//     //   this.addToChat(`${this.state.username} (me)`, msg);
+//     // } else {
+//     //   event.target[0].placeholder = 'Cannot be blank';
+//     // };
+//   };
 
-        </div>
+//   addToChat(name, message) {
+//     let newText = document.createElement('p');
+//     newText.textContent = `${name}: ${message}`;
+//     document.querySelector('#text-chat').appendChild(newText);
+//   };
 
-      </div>
-    );
-  }
-}
+//   onChangeName(event) {
+//     this.setState({username: event.target.value});
+//   };
+
+
+//   componentDidMount() {
+//     const reactThis = this;
+
+//     console.log(this.props);
+
+//     // this.webrtc.on('readyToCall', function () {
+//     //   console.log("Joining room: ", reactThis.props.match.params.roomname);
+//     //   reactThis.webrtc.joinRoom(reactThis.props.match.params.roomname);
+//     // });
+
+//     // this.webrtc.on('channelMessage', function (label, type, data) {
+
+//     //   if (data.type === 'chat') {
+//     //     reactThis.addToChat(data.payload.username, data.payload.message);
+//     //   };
+//     // });
+//   }
+
+
+//   render() {
+
+//     return (
+//       <div className="App">
+
+//         <div className="videoDiv">
+//           <div>
+//             <video id="localVideo"></video>
+//           </div>
+
+//           <div>
+//             <div id="remoteVideos"></div>
+//           </div>
+//         </div>
+
+//         <div className="chatDiv">
+
+//           <div id="text-chat"></div>
+
+//           <span>Your name: </span>
+//           <input onChange={this.onChangeName} value={this.state.username} />
+
+//           <form onSubmit={this.handleChatSubmit}>
+//             <input id="chat-box" placeholder="Enter chat message" />
+//             <input type="submit" value="Send" />
+//           </form>
+
+//         </div>
+
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import { createStore, Actions, Selectors } from "@andyet/simplewebrtc";
 import './index.css';
 
 import Landing from './components/Landing';
 import App from './components/App';
 
 import react_logo from './react_logo.svg';
+
+
+const CONFIG_URL = `https://api.simplewebrtc.com/config/guest/${process.env.REACT_APP_RTC_API_KEY}`
+const store = createStore();
+
+window.store = store;
+window.actions = Actions;
+window.selectors = Selectors;
 
 
 const routing = (
@@ -22,12 +32,18 @@ const routing = (
 
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/:roomname" component={App} />
+        <Route exact path="/:roomname">
+          <Provider store={store}>
+            <App
+              configUrl={CONFIG_URL}
+              roomName={window.location.pathname.split("/").pop()}
+            />
+          </Provider>
+        </Route>
       </Switch>
 
     </div>
   </Router>
 )
-
 
 ReactDOM.render(routing, document.getElementById('root'));
